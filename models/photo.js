86396bf1cb2b575e54f159a162a1be823cb17dab
Thelma Boamah <thelma.a.boamah@gmail.com>
2017-04-11 15:18:40 -0700

    Moved mongoose connection ot db file, added length validation to photo note

--- models/photo.js
+++ models/photo.js
@@ -1,9 +1,16 @@
 var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/myphotoapp', function(err) {
+  if (err) {
+    console.log('Could not connect to mongodb on localhost. Ensure that you have mongodb running on localhost and mongodb accepts connections on standard ports!');
+  }
+});
+
 var photoSchema = new mongoose.Schema({
 	created_at: { type: Date, default: Date.now },
 	url: String,
 	filter: String,
-	note: String //Add length restriction
+	note: { type: String, maxlength: 30 }
 });
 
 var Photo = mongoose.model('Photo', photoSchema);
