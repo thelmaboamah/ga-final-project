c59c1d742cea90901dd1ff914c8110cd5b815658
Thelma Boamah <thelma.a.boamah@gmail.com>
2017-04-11 11:54:54 -0700

    Add routes for photos

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var mongoose = require('mongoose');
 var path = require('path');
 var http = require('http');
 var bodyParser = require('body-parser');
+var Photo = require('./models/photo')
+var photosController = require('./controllers/photo')
 
 var app = express();
 
@@ -20,12 +22,27 @@ mongoose.connect('mongodb://localhost/myphotoapp', function(err) {
   }
 });
 
+//Render homepage
 router.route('/')
 	.get(function(req, res) {
-		res.send("Hello")
+		res.render('index');
+	});
+
+//Render photo display page
+router.route('/photos')
+	.get(function(req, res) {
+		res.render('photos');
 	})
 
 
+router.route('/api/photos')
+	.get(photosController.allPhotos)
+	.post(photosController.savePhoto)
+
+router.route('/api/photos/:id')
+	.get(photosController.onePhoto)
+	.put(photosController.updatePhoto)
+	.delete(photosController.deletePhoto)
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Express server is up and running on http://localhost:3000/');
