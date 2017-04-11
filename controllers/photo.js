1719f8f42456ba2b6fe8c30628d2b7a6af283873
Thelma Boamah <thelma.a.boamah@gmail.com>
2017-04-11 11:55:26 -0700

    Controller functions for CRUDing photos

---
+++ controllers/photo.js
@@ -0,0 +1,55 @@
+var Photo = require('../models/photo')
+
+//Include parameters to order by created_at
+function allPhotos(req, res) {
+	Photo.find({}, function(err, photos) {
+		if (err) {console.log('Could not find all photos', err);}
+		res.json(photos);
+	})
+}
+
+function onePhoto(req, res) {
+	var id = req.params.id;
+
+	Photo.findOne({_id: id}, function(err, photo) {
+		if (err) { console.log ('Could not find specified photo', err);}
+		res.json(photo);
+	})
+}
+
+function savePhoto(req, res) {
+	var photoData = req.body;
+	var photoToSave = new Photo(photoData);
+	photoToSave.save(function(err, savedPhoto){
+		if (err) {console.log('Could not save photo', err)}
+		res.json(savedPhoto);
+	})
+}
+
+function updatePhoto(req, res) {
+	var id = req.params.id;
+	var updates = {
+		filter: req.body.filter,
+		note: req.body.note
+	}
+	Photo.findOneAndUpdate({_id: id}, updates, {new: true}, function(err, updatedPhoto){
+		if (err){ console.log('Could not update photo', err );}
+		res.json(updatedPhoto);
+	})
+}
+
+function deletePhoto(req, res) {
+	var id = req.params.id;
+	Photo.findOneAndRemove({_id: id}, function(err, res) {
+		if (err) {console.log('Could not delete photo', err);}
+		res.sendStatus(204);
+	})
+}
+
+module.exports = {
+	allPhotos: allPhotos,
+	onePhoto: onePhoto,
+	savePhoto: savePhoto,
+	updatePhoto: updatePhoto,
+	deletePhoto: deletePhoto
+}
