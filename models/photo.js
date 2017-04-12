f4dc1245e4e4bcb38fe79d513b3696f54681ee85
Thelma Boamah <thelma.a.boamah@gmail.com>
2017-04-11 22:37:50 -0700

    deploy mongoose connect

--- models/photo.js
+++ models/photo.js
@@ -1,10 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/myphotoapp', function(err) {
-  if (err) {
-    console.log('Could not connect to mongodb on localhost. Ensure that you have mongodb running on localhost and mongodb accepts connections on standard ports!');
-  }
-});
+mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost/myphotoapp');
 
 var photoSchema = new mongoose.Schema({
 	created_at: { type: Date, default: Date.now },
